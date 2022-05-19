50dec9e36a6af63b3c0662edafccfca92e03497d
t-bolt <isholasobuur@gmail.com>
2022-05-19 12:13:36 +0100

    first commit

--- lib/pages/sign_up_page.dart
+++ lib/pages/sign_up_page.dart
@@ -1,1 +1,167 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_assessment/constants/colors.dart';
+import 'package:mobile_assessment/widgets/sign_up_text_fom_field.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final TextEditingController _hospitalNameController = TextEditingController();
+  final TextEditingController _hospitalAddressController =
+      TextEditingController();
+  final TextEditingController _hospitalContactController =
+      TextEditingController();
+  final TextEditingController _gasQuantityController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  bool _isPasswordVisble = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+        title: const Text(
+          'SignUp',
+          style: TextStyle(
+            color: kHeaderTextColor,
+            fontSize: 28.0,
+          ),
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Padding(padding: EdgeInsets.only(top: 30.0)),
+          const Text(
+            'Hospital Name',
+            style: TextStyle(
+              color: kTextFieldColor,
+              fontSize: 28.0,
+            ),
+          ),
+          const Padding(padding: EdgeInsets.only(bottom: 5.0)),
+          SignUpTextFormField(
+            controller: _hospitalNameController,
+            textInputType: TextInputType.text,
+            obscureText: false,
+            validator: (String? text) {
+              if (text == null || text.isEmpty) {
+                return 'Field is required';
+              } else {
+                return null;
+              }
+            },
+          ),
+          const Padding(padding: EdgeInsets.only(bottom: 10.0)),
+          const Text(
+            'Hospital Address',
+            style: TextStyle(
+              color: kTextFieldColor,
+              fontSize: 28.0,
+            ),
+          ),
+          const Padding(padding: EdgeInsets.only(bottom: 5.0)),
+          SignUpTextFormField(
+            controller: _hospitalAddressController,
+            textInputType: TextInputType.text,
+            obscureText: false,
+            validator: (String? text) {
+              if (text == null || text.isEmpty) {
+                return 'Field is required';
+              } else {
+                return null;
+              }
+            },
+          ),
+          const Padding(padding: EdgeInsets.only(bottom: 10.0)),
+          const Text(
+            'Hospital Contact',
+            style: TextStyle(
+              color: kTextFieldColor,
+              fontSize: 28.0,
+            ),
+          ),
+          const Padding(padding: EdgeInsets.only(bottom: 5.0)),
+          SignUpTextFormField(
+            controller: _hospitalContactController,
+            textInputType: TextInputType.number,
+            obscureText: false,
+            validator: (String? text) {
+              if (text == null || text.isEmpty) {
+                return 'Field is required';
+              } else {
+                return null;
+              }
+            },
+          ),
+          const Padding(padding: EdgeInsets.only(bottom: 10.0)),
+          const Text(
+            'Gas Quantity',
+            style: TextStyle(
+              color: kTextFieldColor,
+              fontSize: 28.0,
+            ),
+          ),
+          const Padding(padding: EdgeInsets.only(bottom: 5.0)),
+          SignUpTextFormField(
+            controller: _gasQuantityController,
+            textInputType: TextInputType.number,
+            obscureText: false,
+            validator: (String? text) {
+              if (text == null || text.isEmpty) {
+                return 'Field is required';
+              } else {
+                return null;
+              }
+            },
+          ),
+          const Padding(padding: EdgeInsets.only(bottom: 10.0)),
+          const Text(
+            'Password',
+            style: TextStyle(
+              color: kTextFieldColor,
+              fontSize: 28.0,
+            ),
+          ),
+          const Padding(padding: EdgeInsets.only(bottom: 5.0)),
+          SignUpTextFormField(
+            controller: _passwordController,
+            textInputType: TextInputType.visiblePassword,
+            obscureText: false,
+            suffixIcon: IconButton(
+              onPressed: () {
+                setState(() {
+                  _isPasswordVisble = !_isPasswordVisble;
+                });
+              },
+              icon: _isPasswordVisble
+                  ? const Icon(Icons.visibility, color: Colors.black)
+                  : const Icon(
+                      Icons.visibility_off,
+                      color: Colors.black,
+                    ),
+            ),
+            validator: (String? text) {
+              if (text == null || text.isEmpty) {
+                return 'Field is required';
+              } else if (text.length < 8) {
+                return 'Password must be at least 8 characters';
+              } else {
+                return null;
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/sign_up_text_fom_field.dart
+++ lib/widgets/sign_up_text_fom_field.dart
@@ -1,1 +1,54 @@
 import 'package:flutter/material.dart';
+
+class SignUpTextFormField extends StatelessWidget {
+  final TextEditingController controller;
+  final TextInputType textInputType;
+  final bool obscureText;
+  final String? Function(String?) validator;
+  final Widget? suffixIcon;
+
+  const SignUpTextFormField({
+    Key? key,
+    required this.controller,
+    required this.textInputType,
+    required this.obscureText,
+    required this.validator,
+    this.suffixIcon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: obscureText,
+      style: const TextStyle(
+        color: Colors.blueAccent,
+        fontSize: 16.0,
+      ),
+      keyboardType: textInputType,
+      decoration: InputDecoration(
+        suffixIcon: suffixIcon,
+        fillColor: const Color(0xFFF5F5F5),
+        filled: true,
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12.0),
+          borderSide: const BorderSide(
+            color: Color(0xFFF5F5F5),
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12.0),
+          borderSide: const BorderSide(
+            color: Colors.blueAccent,
+          ),
+        ),
+        border: OutlineInputBorder(
+          borderSide: const BorderSide(
+            color: Color(0xFFF5F5F5),
+          ),
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+      ),
+    );
+  }
+}
